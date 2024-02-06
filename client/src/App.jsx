b2d4c13c52045e86244837e8c91b931b29f1508d
sripathman <nsripathman@gmail.com>
2024-02-06 10:57:47 +0000

    create Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,40 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Search from './pages/Search';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Projects from './pages/Projects';
+//import Header from '../components/Header'
+
+
+
+
+
 export default function App() {
   return (
+    <BrowserRouter>
+
+
     
-    <div class="bg-gradient-to-r from-green-500 via-cyan-500 to-blue-500 text-red-500">hello world</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+
+
+      
+      
+
+      
+    </Routes>
+  
+  </BrowserRouter>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>Home</div>
+  )
+}
